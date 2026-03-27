0e4e700207fa2a29b7b4e1ea89b2c30ada7d4432
aeskrynchenko78 <aeskrynchenko78@gmail.com>
2026-03-27 13:44:35 +0300

    Настроен ESLint 9.x с @stylistic

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/'],
+  },
+];
